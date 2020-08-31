a7571de8206f9315e32101e97b8fc9d8d838783c
DavidS <dsanchezsanc@uoc.edu>
2020-08-31 15:43:58 +0200

    reestructurado de datos y volcado completo

--- test.py
+++ test.py
@@ -1,11 +1,92 @@
 import pyodbc
 
-#debemos usar un interprete de 64bits
+# debemos usar un interprete de 64bits
 conn = pyodbc.connect(
-    r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=.\db\BD_Artardor.accdb;')
+    r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=.\db\BD_Artardor_2.accdb;')
 
 cursor = conn.cursor()
-cursor.execute("select * from Artistes")
 
+print("TABLA")
+cursor.execute("select * from Participacions")
 for row in cursor.fetchall():
     print(row)
+
+# borramos todos los registros de la tabla Participacions
+cursor.execute("delete from Participacions")
+conn.commit()
+
+print("TABLA")
+cursor.execute("select * from Participacions")
+for row in cursor.fetchall():
+    print(row)
+
+cursor.execute("select * from Artistes")
+for row in cursor.fetchall():
+
+    id_artista = row[0]
+
+    try:
+        int(id_artista)
+    except ValueError:
+        continue
+
+    nom_artista = row[1]
+
+    exp = ("select * from Participants where NOM_ARTISTA = '" + nom_artista + "'")
+    cursor.execute(exp)
+
+    for row in cursor.fetchall():
+
+        anys_participacio = str.split(row[4], ";")
+        modalitats = str.split(row[5], ";")
+
+        len_anys_participacio = len(anys_participacio)
+        len_modalitats = len(modalitats)
+
+        if len_modalitats == 0: continue
+        if len_anys_participacio == 0: continue
+
+        print('****************************************************')
+        print(len_anys_participacio, len_modalitats)
+        if len_anys_participacio == len_modalitats:
+            for any_participacio,modalitat in zip(anys_participacio,modalitats):
+                print(any_participacio, modalitat)
+                try:
+                    int(any_participacio)
+                except ValueError:
+                    continue
+
+                cursor.execute('''INSERT INTO Participacions (id_artista,any,modalitat) VALUES (?,?,?)''', (int(id_artista), int(any_participacio), modalitat))
+                # exp = "INSERT INTO Participacions (id_artista, any_participacio, modalitat) VALUES ('" + str(id_artista) + "','" + str(any_participacio) + "', '" + modalitat + "'"
+
+        if (len_anys_participacio > 1) & (len_modalitats == 1):
+            for any_participacio in anys_participacio:
+                print(any_participacio, modalitats[0])
+
+                try:
+                    int(any_participacio)
+                except ValueError:
+                    continue
+
+                cursor.execute('''INSERT INTO Participacions (id_artista,any,modalitat) VALUES (?,?,?)''', (int(id_artista), int(any_participacio), modalitats[0]))
+                # exp = "INSERT INTO Participacions (id_artista, any_participacio, modalitat) VALUES ('" + str(id_artista) + "','" + str(any_participacio) + "', '" + modalitats[0] + "'"
+
+        if (len_anys_participacio == 1) & (len_modalitats > 1):
+            for modalitat in modalitats:
+                print(anys_participacio[0], modalitat)
+
+                try:
+                    int(anys_participacio[0])
+                except ValueError:
+                    continue
+
+                cursor.execute('''INSERT INTO Participacions (id_artista,any,modalitat) VALUES (?,?,?)''', (int(id_artista), int(anys_participacio[0]), modalitat))
+                # exp = "INSERT INTO Participacions (id_artista, any_participacio, modalitat) VALUES ('" + str(id_artista) + "','" + str(anys_participacio[0]) + "', '" + modalitat + "'"
+
+        if (len_anys_participacio > 1) & (len_modalitats > 1) & (len_anys_participacio != len_modalitats):
+            # imposible asignar
+            print('imposible asignar')
+            print(anys_participacio, modalitats)
+            pass
+
+        conn.commit()
